cd36c159c46fd863f6044737aab6c9ac1c761aa3
Aguh Lewis <aguhlewis@gmail.com>
2018-07-01 21:20:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,23 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
 	event.waitUntil(
-		caches.open('alc-with-google3.0').then(function(cache) {
-			return cache.addAll([
-				'css/bootstrap.min.css',
-				'css/font-awesome.css',
-				'css/style.css',
-				'js/bootstrap.min.js',
-				'js/script.js',
-				'index.html'
-			]);
-		})
+		caches.open('alc-with-google3.0').then(cache => cache.addAll([
+            'css/bootstrap.min.css',
+            'css/font-awesome.css',
+            'css/style.css',
+            'js/bootstrap.min.js',
+            'js/script.js',
+            'index.html'
+        ]))
 	);
 });
 
 /*fetch the page from the cache. if request doesn't match any in the catch:
 get it from the network, send it to the page and add it to the cache at the same time*/
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
 	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request).then(function(response) {
-				cache.put(event.request, response.clone());
-				return response;
-			});
-		})
+		caches.match(event.request).then(response => response || fetch(event.request).then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+        }))
 	);
 });
